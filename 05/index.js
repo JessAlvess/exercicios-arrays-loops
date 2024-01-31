7dd7984fd629b7708521db3679cc8c74b859b4bf
Jessé Alves <jessealvespr@outlook.com>
2024-01-31 20:59:30 -0300

    feat: finalizado exercicio 5

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,10 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+const pares = [];
+
+original.forEach(numero => {
+    if (numero%2 === 0) {
+        pares.push(numero)
+    }
+});
+
+console.log(pares);
