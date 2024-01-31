9bc896b21c100aef21126b9b9733816912f5f61b
Jessé Alves <jessealvespr@outlook.com>
2024-01-31 20:29:47 -0300

    feat: finalizado exercicio 2

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,21 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+// const letras = ["A", "a", "B", "C", "E", "e"];
+let soma = 0;
+let encontrado = false;
+
+const letras = ["A", "a", "B", "C", "L", "z"];
+
+
+
+for (const letra of letras) {
+    if (letra === 'e' || letra === 'E') {
+        soma += 1;
+        encontrado = true;
+    }
+}
+
+if (encontrado) {
+    console.log(`Foram encontradas ${soma} letras "E" ou "e".`);
+} else {
+    console.log(`Nenhuma letra "E" ou "e" foi encontrada.`);
+}
+
