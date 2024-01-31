277f559663f7957a6ab023c864d0bcbbcbdf1892
Jessé Alves <jessealvespr@outlook.com>
2024-01-31 20:38:17 -0300

    feat: finalizado exercicio 3

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,20 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+// const numeros = [54, 12, 14, 87, 10, 284];
+let encontrado = false;
+let indice;
 
+// const numeros = [54, 22, 14, 10, 284];
+const numeros = [54, 22, 14, 87, 284];
+
+for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] === 10 ) {
+        encontrado = true;
+        indice = index;
+        break;
+    }
+}
+
+if (encontrado) {
+    console.log(indice);
+} else {
+    console.log(-1);
+}
