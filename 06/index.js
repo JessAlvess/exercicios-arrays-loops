ab009d965c5c449a50ced5981dd5511a02c77e80
Jessé Alves <jessealvespr@outlook.com>
2024-01-31 21:02:58 -0300

    feat: finalizado exercicio 6

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,15 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const pares = [];
+let soma = 0;
 
+numeros.forEach(numero => {
+    if (numero %2 === 0) {
+        pares.push(numero)        
+    }
+})
+
+for (const numero of pares) {
+    soma += numero;
+}
+
+console.log(soma);
