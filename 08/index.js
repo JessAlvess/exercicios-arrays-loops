aa8b4ae6713e7001a758e10e1f9efc30229f389f
Jessé Alves <jessealvespr@outlook.com>
2024-01-31 21:10:37 -0300

    feat: finalizado exercicio 8

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,9 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+// const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [3, 4, 1, 8, 11, 7, 5];
+let maiorNumero = -Infinity;
+
+for (const number of numeros) {
+    maiorNumero = number > maiorNumero? number : maiorNumero;    
+}
+
+console.log(maiorNumero);
